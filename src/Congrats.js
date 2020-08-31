aa7c4aecc76ebdbf7a8b51b995e8d0d2e291444a
alirni <aliramezani19.1@gmail.com>
2020-08-31 11:11:00 +0430

    feat(Congrats): create new component

---
+++ src/Congrats.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component
+ */
+export default () => {
+  return <div></div>;
+};
