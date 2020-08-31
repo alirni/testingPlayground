f6f51b7bbb4552f4aacc5dada980bddfdb1e8a82
alirni <aliramezani19.1@gmail.com>
2020-08-31 12:23:14 +0430

    fix(Congrats): refactor for pass tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -5,6 +5,16 @@ import React from 'react';
  * @function
  * @returns {JSX.Element} - Rendered component
  */
-export default () => {
-  return <div></div>;
+export default props => {
+  const { success } = props;
+
+  if (success) {
+    return (
+      <div data-test="componentCongrats">
+        <span data-test="congratsMessage">Congratulations! You guessed the word!</span>
+      </div>
+    );
+  } else {
+    return <div data-test="componentCongrats"></div>;
+  }
 };
