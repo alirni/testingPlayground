d1949bb22f42ea29f4c7e93d099455a3f250a99e
alirni <aliramezani19.1@gmail.com>
2020-08-31 12:16:22 +0430

    fix(Congrats.test): write test

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -17,8 +17,20 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
 };
 
-test('renders without error', () => {});
+test('renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'componentCongrats');
+  expect(component.length).toBe(1);
+});
 
-test('renders no text when `success` prop is false', () => {});
+test('renders no text when `success` prop is false', () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, 'componentCongrats');
+  expect(component.text()).toBe('');
+});
 
-test('renders non-empty Congrats message when `success` prop is true', () => {});
+test('renders non-empty Congrats message when `success` prop is true', () => {
+  const wrapper = setup({ success: true });
+  const component = findByTestAttr(wrapper, 'congratsMessage');
+  expect(component.text().length).not.toBe(0);
+});
