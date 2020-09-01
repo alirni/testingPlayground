a5621c568e191b8c9dfdfb2fca35b8ae6dbd688e
alirni <aliramezani19.1@gmail.com>
2020-09-01 12:00:46 +0430

    setup redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - Action to be reduced.
+ * @returns {boolean} - New success state.
+ */
+export default (state, action) => {
+  return null;
+};
