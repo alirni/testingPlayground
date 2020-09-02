bb050c663ac3de7689daad4b846fe4e7281690db
alirni <aliramezani19.1@gmail.com>
2020-09-02 11:29:19 +0430

    fix: install ReduxThunk and use middleware

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,16 +1,19 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 /**
  * Create a testing store with imported reducer, middleware, and initial state.
+ *  globals: rootReducer, middlewares.
  * @function storeFactory
  * @param {object} initialState - Initial state store.
  * @returns {store} - Redux store.
  */
 export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
