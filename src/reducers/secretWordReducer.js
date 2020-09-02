17e0593ed2d3ecf177ced968d46f03707e5669b3
alirni <aliramezani19.1@gmail.com>
2020-09-02 17:07:05 +0430

    feat(secretWordReducer)

---
+++ src/reducers/secretWordReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function secretWordReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - Action to be reduced.
+ * @returns {array} - New guessedWords state.
+ */
+export default (state = null, action) => {
+  return state;
+};
