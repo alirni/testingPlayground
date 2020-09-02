c06a29208d7b3fecd8431e48c10ba6370269d548
alirni <aliramezani19.1@gmail.com>
2020-09-02 12:50:14 +0430

    fix: create guessWord function and setup thunk

---
+++ src/integration.test.js
@@ -0,0 +1,14 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('update state correctly for unsuccessful guess', () => {});
+    test('update state correctly for successful guess', () => {});
+  });
+
+  describe('some guessed words', () => {
+    test('update state correctly for unsuccessful guess', () => {});
+    test('update state correctly for successful guess', () => {});
+  });
+});
---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function guessedWordReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - Action to be reduced.
+ * @returns {array} - New guessedWords state.
+ */
+export default (state, action) => {
+  return null;
+};
