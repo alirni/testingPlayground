616527d61233e46c693f4a0de4e8f93041d1b0e1
alirni <aliramezani19.1@gmail.com>
2020-09-02 17:08:46 +0430

    fix(guessedWordsReducer): add guessedWords reducer

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,9 +1,16 @@
+import { actionTypes } from '../actions/index';
+
 /**
- * @function guessedWordReducer
+ * @function guessedWordsReducer
  * @param {array} state - Array of guessed words.
  * @param {object} action - Action to be reduced.
  * @returns {array} - New guessedWords state.
  */
-export default (state, action) => {
-  return null;
+export default (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
