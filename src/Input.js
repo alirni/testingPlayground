c16ba694d353c7fb2bff8eafbc5c12c51e324236
alirni <aliramezani19.1@gmail.com>
2020-09-03 11:36:56 +0430

    fix(Input.test): add redux props test

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { guessWord } from './actions';
+
 class Input extends Component {
   render() {
     const { success } = this.props;
@@ -21,4 +23,4 @@ const mapStateToProps = ({ success }) => {
   return { success };
 };
 
-export default connect(mapStateToProps)(Input);
+export default connect(mapStateToProps, { guessWord })(Input);
