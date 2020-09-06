c2b87dd1d75aeb7bc1398805f684036783ffb713
alirni <aliramezani19.1@gmail.com>
2020-09-06 09:33:06 +0430

    fix(App): add useEffect and useReducer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,35 @@ import React from 'react';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
 import Input from './Input';
-// import { getSecretWord } from './actions';
+import hookActions from './actions/hookActions';
+
+/**
+ *
+ * @param {object} state
+ * @param {object} action - contains 'type' and 'payload' properties for state update
+ * @example                 { type: 'setSecretWord', payload: 'party' }
+ * @returns {object} - new state
+ */
+function reducer(state, action) {
+  switch (action.type) {
+    case 'setSecretWord':
+      return { ...state, secretWord: action.payload };
+
+    default:
+      throw new Error(`Invalid action typr: ${action.type}`);
+  }
+}
 
 const App = () => {
   // const { success, guessedWords, secretWord } = this.props;
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = secretWord => dispatch({ type: 'setSecretWord', payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
   return (
     <div className="App" data-test="componentApp">
       <h1>Jotto</h1>
